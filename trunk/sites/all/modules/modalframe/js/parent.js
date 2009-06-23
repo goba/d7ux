26de2cf3436bce9c132d886b916b253b25acd9e0
cwgordon7@cwgordon.com <cwgordon7@cwgordon.com@7f14cd9e-56b5-11de-bc7c-c9df4fa8b3c6>
2009-06-23 13:34:43 +0000

    Added drop shadows.

--- trunk/sites/all/modules/modalframe/js/parent.js
+++ trunk/sites/all/modules/modalframe/js/parent.js
@@ -93,6 +93,7 @@ Drupal.modalFrame.create = function() {
       // Compute frame size and dialog position based on dialog size.
       var frameSize = $.extend({}, dialogSize);
       frameSize.height -= $('.modalframe .ui-dialog-titlebar').outerHeight(true);
+      frameSize.height -= 15;
       var dialogPosition = self.computeCenterPosition($('.modalframe'), dialogSize);
 
       // Adjust size of the iframe element and container.
@@ -358,6 +359,10 @@ Drupal.modalFrame.bindChild = function(iFrameWindow, isClosing) {
       }
     });
 
+    if (!$('#modalframe-container').find('div.shadow').size()) {
+      $('#modalframe-container').append('<div class="shadow"/>');
+    }
+
     var tabs = $iFrameDocument.find('ul.horizontal-tabs-panes, ul.primary').get(0);
 
     // If there are tabs in the page, move them to the titlebar.
@@ -498,6 +503,7 @@ Drupal.modalFrame.resize = function(size) {
   //  dialogSize.height = maxSize.height;
   //  frameSize.height = dialogSize.height - titleBarHeight;
   //}
+  frameSize.height = frameSize.height - 15;
 
   // Compute dialog position centered on viewport.
   var dialogPosition = self.computeCenterPosition($('.modalframe'), dialogSize);
