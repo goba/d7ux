85e842a28a43471123ba240c14dc9af8f203b54a
gabor.hojtsy <gabor.hojtsy@7f14cd9e-56b5-11de-bc7c-c9df4fa8b3c6>
2009-07-01 12:00:17 +0000

    Adding changes documented at http://drupal.org/node/484820#comment-1763592 and http://drupal.org/node/507072 (initial patch)

--- trunk/modules/admin/admin-toolbar.tpl.php
+++ trunk/modules/admin/admin-toolbar.tpl.php
@@ -3,28 +3,29 @@
 
 /**
  * @file
- * Default template for header admin toolbar.
+ * Default template for admin toolbar.
  *
  * Available variables:
- * - $collapsed: Boolean for whether the admin links should be collapsed.
+ * - $collapsed: Boolean for whether the admin shortcuts should be collapsed.
  * - $user_menu: User account / logout links.
- * - $admin_menu: Management menu links.
- * - $admin_links: Convenience links from the "Admin" menu.
+ * - $admin_menu: Top level management menu links.
+ * - $admin_shortcuts: Convenience shortcuts from the "Admin" menu 
+ *   (shown if not $collapsed).
  *
  * @see template_preprocess()
  * @see template_preprocess_admin_toolbar()
  */
 ?>
-<div id='admin-toolbar' class='clearfix'>
-  <div class='admin-menu clearfix'>
-    <span class='toggle <?php if (!$collapsed) print 'toggle-active' ?>'><?php print t('Admin links') ?></span>
+<div id="admin-toolbar" class="clearfix">
+  <div class="admin-menu clearfix">
+    <span class="toggle <?php if (!$collapsed) print 'toggle-active' ?>"><?php print t('Show shortcuts') ?></span>
     <?php print $user_menu ?>
     <?php print $admin_menu ?>
   </div>
 
-  <div class='admin-links clearfix <?php if ($collapsed) print 'collapsed' ?>'>
-    <?php print $admin_links; ?>
+  <div class="admin-shortcuts clearfix <?php if ($collapsed) print 'collapsed' ?>">
+    <?php print $admin_shortcuts; ?>
   </div>
 
-  <div class='shadow'></div>
+  <div class="shadow"></div>
 </div>
--- trunk/modules/admin/admin_toolbar.js
+++ trunk/modules/admin/admin_toolbar.js
@@ -6,12 +6,14 @@
  */
 Drupal.behaviors.admin = {
   attach: function() {
+
     // Set the intial state of the toolbar
     $('#admin-toolbar:not(.processed)').each(function() {
       Drupal.admin.toolbar.init();
       $(this).addClass('processed');
     });
-    // Toggling of admin link visibility.
+
+    // Toggling of admin shortcuts visibility.
     $('#admin-toolbar span.toggle:not(.processed)').each(function() {
       $(this).click(function() {
         Drupal.admin.toolbar.toggle();
@@ -23,8 +25,7 @@ Drupal.behaviors.admin = {
 };
 
 /**
- * Initialize the admin object cautiously to avoid collisions with other
- * modules (admin_menu).
+ * Initialize cautiously to avoid collisions with other modules (admin_menu).
  */
 Drupal.admin = Drupal.admin || {};
 Drupal.admin.toolbar = Drupal.admin.toolbar || {};
@@ -34,19 +35,7 @@ Drupal.admin.toolbar = Drupal.admin.toolbar || {};
  */
 Drupal.admin.toolbar.init = function() {
   // Retrieve the collapsed status from a stored cookie.
-  var collapsed = null;
-  var name = 'Drupal.admin.toolbar.collapsed';
-  if (document.cookie && document.cookie != '') {
-    var cookies = document.cookie.split(';');
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = jQuery.trim(cookies[i]);
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) == (name + '=')) {
-        collapsed = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
-    }
-  }
+  var collapsed = $.cookies.get('Drupal.admin.toolbar.collapsed');
 
   // Expand or collapse the toolbar based on the cookie value.
   if (collapsed == 1) {
@@ -61,29 +50,35 @@ Drupal.admin.toolbar.init = function() {
  * Collapse the admin toolbar.
  */
 Drupal.admin.toolbar.collapse = function() {
-  $('#admin-toolbar div.admin-links').addClass('collapsed');
+  $('#admin-toolbar div.admin-shortcuts').addClass('collapsed');
   $('#admin-toolbar span.toggle').removeClass('toggle-active');
-  $('body').removeClass('admin-toolbar-links');
-
-  document.cookie = ['Drupal.admin.toolbar.collapsed', '=', encodeURIComponent(1), '; expires=-1', '; path='+ Drupal.settings.basePath].join('');
+  $('body').removeClass('admin-toolbar-shortcuts');
+  $.cookies.set(
+    'Drupal.admin.toolbar.collapsed', 
+    1, 
+    {path: Drupal.settings.basePath}
+  );
 }
 
 /**
  * Expand the admin toolbar.
  */
 Drupal.admin.toolbar.expand = function() {
-  $('#admin-toolbar div.admin-links').removeClass('collapsed');
+  $('#admin-toolbar div.admin-shortcuts').removeClass('collapsed');
   $('#admin-toolbar span.toggle').addClass('toggle-active');
-  $('body').addClass('admin-toolbar-links');
-
-  document.cookie = ['Drupal.admin.toolbar.collapsed', '=', encodeURIComponent(0), '; expires=-1', '; path='+ Drupal.settings.basePath].join('');
+  $('body').addClass('admin-toolbar-shortcuts');
+  $.cookies.set(
+    'Drupal.admin.toolbar.collapsed', 
+    0, 
+    {path: Drupal.settings.basePath}
+  );
 }
 
 /**
  * Toggle the admin toolbar.
  */
 Drupal.admin.toolbar.toggle = function() {
-  if ($('#admin-toolbar div.admin-links').is('.collapsed')) {
+  if ($('#admin-toolbar div.admin-shortcuts').is('.collapsed')) {
     Drupal.admin.toolbar.expand();
   }
   else {
