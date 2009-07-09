858159481e05889d6b0643236e9dde82d3fca909
gabor.hojtsy <gabor.hojtsy@7f14cd9e-56b5-11de-bc7c-c9df4fa8b3c6>
2009-07-09 19:15:38 +0000

    Apply table and pager styling from the mockpups from http://farm4.static.flickr.com/3343/3569318373_b4422fafae_o.jpg Also seen on http://d7ux.org/content in smaller size
    In short:
     - fix link color once and for all to that used on the mockups
     - made header and even rows use the provided colors
     - fix header font sizing and bolding
     - fix cell spacing to add some space making columns easier to identify
     - applied similar borders to what is designed (not pixel perfectly as designed lacking some classes on table header cells)
     - apply active column styling as designed
     - introduce black table header ordering markers and place them appropriately on the righ of the table header cell
     - made the whole area of the column headers clickable when orderable (via display block on the containing links), so that the whole cell can be clicked to reoder (makes it way easier to reorder)
     - applied pager styling which also required restoring a Drupal 6 feature recently removed from Drupal 7 (patch to add it back is at http://drupal.org/node/514914)
    
    Not implemented as designed:
     - some (minimal) parts of the border due to lack of classes markin the first and last header cell
     - table headers which *can* be used for ordering need to have a different color (since not all headers can be used for that); mockups use all  - table headers which *can* be usedoversight

--- trunk/themes/slate/template.php
+++ trunk/themes/slate/template.php
@@ -83,3 +83,56 @@ function slate_button($element) {
 
   return '<button type="submit" ' . (empty($element['#name']) ? '' : 'name="' . $element['#name'] . '" ') . 'id="' . $element['#id'] . '" value="' . check_plain($element['#value']) . '" ' . drupal_attributes($element['#attributes']) . "><span>" . check_plain($element['#value']) . "</span></button>\n";
 }
+
+/**
+ * Override of theme_tablesort_indicator().
+ *
+ * Use our own image versions, so they show up as black and not gray on gray.
+ */
+function slate_tablesort_indicator($style) {
+  $theme_path = drupal_get_path('theme', 'slate');
+  if ($style == "asc") {
+    return theme('image', $theme_path . '/images/arrow-asc.png', t('sort icon'), t('sort ascending'));
+  }
+  else {
+    return theme('image', $theme_path . '/images/arrow-desc.png', t('sort icon'), t('sort descending'));
+  }
+}
+
+/**
+ * Override of theme_pager().
+ *
+ * Implement "Showing 1-50 of 2345  Next 50 >" type of output.
+ */
+function slate_pager($tags = array(), $element = 0, $parameters = array(), $quantity = 9) {
+  global $pager_page_array, $pager_total, $pager_total_items, $pager_limits;
+  
+  $total_items = $pager_total_items[$element];
+  
+  if ($total_items == 0) {
+    // No items to display.
+    return;
+  }
+  
+  $total_pages = $pager_total[$element];
+  $limit = $pager_limits[$element];
+  $showing_min = $pager_page_array[$element] * $limit + 1;
+  $showing_max = min(($pager_page_array[$element] + 1) * $limit, $total_items);
+  $pager_current = $pager_page_array[$element];
+
+  $output = '<div class="short-pager">';
+  if ($pager_current > 0) {
+    $page_new = pager_load_array($pager_current - 1, $element, $pager_page_array);
+    $output .= '<div class="short-pager-prev">' . theme('pager_link', t('Previous @limit', array('@limit' => $limit)), $page_new, $element, $parameters, array('title' => t('Go to the previous page'))) . '</div>';
+  }
+
+  $output .= '<div class="short-pager-main">' . t('Showing @range <span class="short-pager-total">of @total</span>', array('@range' => $showing_min . ' - ' . $showing_max, '@total' => $total_items)) . '</div>';
+
+  if (($pager_current < ($total_pages - 1)) && ($total_pages > 1)) {
+    $page_new = pager_load_array($pager_current + 1, $element, $pager_page_array);
+    $output .= '<div class="short-pager-next">' . theme('pager_link', t('Next @limit', array('@limit' => $limit)), $page_new, $element, $parameters, array('title' => t('Go to the next page'))) . '</div>';
+  }
+  $output .= '</div>';
+  
+  return $output;
+}
